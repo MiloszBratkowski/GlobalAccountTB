fc440db97395893ae0ec4f242a79ba6b56ea0c5b
MiloszBratkowski <76225054+MiloszBratkowski@users.noreply.github.com>
2023-03-26 11:40:00 +0200

    Initial commit

---
+++ src/main/java/pl/techbrat/spigot/globalaccounttb/GlobalAccountTB.java
@@ -0,0 +1,17 @@
+package pl.techbrat.spigot.globalaccounttb;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GlobalAccountTB extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
